fc9d13e9a59f90e5bee16f63e754265a0714646d
Abraham <abrahambaos@gmail.com>
2021-04-16 13:31:13 +0200

    Puesto el constructor en hexagono.java

--- src/Hexagono.java
+++ src/Hexagono.java
@@ -1,1 +1,23 @@
 //Hexagonos
+/**
+ * 
+ */
+
+/**
+ * @author b09-05Bm
+ *
+ */
+public class Hexagonos extends Poligonos {
+
+	public Hexagonos(long color, double lado) {
+		super(color, lado);
+	}
+	
+	public Hexagonos(double lado) {
+		super(lado);
+	}
+	
+	public Hexagonos() {
+		super();
+	}
+}
